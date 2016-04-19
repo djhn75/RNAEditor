3c1adfbcb8c0f126a83ac2d1c153e68f2f434293
David John <John@med.uni-frankfurt.de>
2016-04-19 11:34:23 +0200

    added setup for cxfreeze

---
+++ setup-cxfreeze.py
@@ -0,0 +1,20 @@
+#import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+
+#base = 'Win32GUI' if sys.platform=='win32' else None
+base=None
+
+executables = [
+    Executable('Main.py', base=base, targetName = 'RnaEditor')
+]
+
+setup(name='RnaEditor',
+      version = '1.0',
+      description = 'A tool to detect editing events from rna-seq data.',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
