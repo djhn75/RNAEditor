1c3684e731c1bf8104892b42cbb4ae47affff035
David John <John@med.uni-frankfurt.de>
2016-04-22 18:35:02 +0200

    added webiste to repository

---
+++ website/about.php
@@ -0,0 +1,63 @@
+<html>
+	<?php
+        /* Write Header which includes all scripts and Stylesheets */
+        include ('html/header.html');
+    ?>
+	
+	<body>
+	<img src="img/rnaEditor_64x64.png" style="position: absolute;" id="waitImg" width=50"/>
+       <div class="row">
+       	<div class="small-12 large-12 columns">
+            <?php
+                /* Write Menu */
+                include ('html/menu.html');
+            ?>
+		 </div>
+		</div>
+		
+		<div class="row">
+        	<div class="small-12 large-12 columns">    
+            
+	            <div class="content">
+					
+						
+						<h3>About</h3>
+						
+						
+						
+						<div class='panel'>
+							<h4>Contact Information</h4>
+								<ul>
+									<li>David John (john@med.uni-frankfurt.de)</li>
+									<li>Dr. Shizuka Uchida (heart.lncrna@gmail.com)</li>
+								</ul>
+							
+							<h4>Funding</h4>
+								<p>This work was supported by the LOEWE Center for Cell and Gene Therapy (State of Hessen), the DFG (SFB834), and the German Center for Cardiovascular Research (DZHK).</p>
+								
+							<h4>Legal Statement</h4>
+								<p>RnaEditor is free to use, 
+								provided that the original work is properly cited. 
+								It is provided "as is" without any reliability whatsoever. 
+								We have taken extreme care regarding the contents that we provide in RnaEditor, 
+								but if you identify a bug, please contact us. If you are commercial user, 
+								please contact us: heart.lncrna@gmail.com</p>
+	
+				
+						
+					</div>
+						
+						
+	            </div>
+			</div>
+		</div>
+
+
+		<?php
+                /* Write Menu */
+                include ('html/footer.html');
+            ?>
+	</body>
+
+
+</html>
---
+++ website/gettingStarted.php
@@ -0,0 +1,43 @@
+<html>
+	<?php
+        /* Write Header which includes all scripts and Stylesheets */
+        include ('html/header.html');
+    ?>
+	
+	<body>
+	<img src="img/rnaEditor_64x64.png" style="position: absolute;" id="waitImg" width=50"/>
+       <div class="row">
+       	<div class="small-12 large-12 columns">
+            <?php
+                /* Write Menu */
+                include ('html/menu.html');
+            ?>
+		 </div>
+		</div>
+		
+		<div class="row">
+        	<div class="small-12 large-12 columns">    
+            
+	            <div class="content">
+					
+						
+						<h3>RnaEditor Manual</h3>
+						
+						<div class='panel'>
+							<p>Manual page content</p>
+								
+						
+						</div>
+	            </div>
+			</div>
+		</div>
+
+
+		<?php
+                /* Write Menu */
+                include ('html/footer.html');
+            ?>
+	</body>
+
+
+</html>
